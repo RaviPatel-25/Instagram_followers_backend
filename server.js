01cb007e152bbfc21d679d7fb7f0bb926d02091f
Ravi Patel <ravipatel.25.02.2005@gmail.com>
2026-02-12 13:24:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://indian-instagram-followers.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
